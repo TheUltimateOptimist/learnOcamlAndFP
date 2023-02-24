31182a22d535f11c9b672a38086ef8842b343f90
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-24 14:50:57 +0100

    advanced pattern matching

---
+++ mons.ml
@@ -0,0 +1,24 @@
+type ptype = TNormal | TFire | TWater
+type peff = ENormal | ENotVery | ESuper
+
+let mult_of_eff = function
+  ENormal -> 1.
+  | ENotVery -> 0.5
+  | ESuper -> 2.0
+
+let eff = function
+  | (TFire, TFire) | (TWater, TWater) | (TFire, TWater) -> ENotVery
+  | (TWater, TFire) -> ESuper
+  | _ -> ENormal
+
+type mon = {
+  name: string;
+  hp: int;
+  ptype: ptype;
+}
+
+let  charmander = {
+  name = "Charmander";
+  hp = 39;
+  ptype = TFire;
+}
