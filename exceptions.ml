e32c053002646c8f611b2de741dda6b6aa057b11
TheUltimateOptimist <digitalConfidence@web.de>
2023-03-01 16:27:07 +0100

    finished exceptions.ml

--- exceptions.ml
+++ exceptions.ml
@@ -4,3 +4,10 @@ exception ExceptionTwo of string
 let save_div x y =
   try x / y with
   | Division_by_zero -> 0
+
+let v = match List.hd [] with
+| [] -> "empty"
+| _ :: _ -> "nonempty"
+| exception Failure s-> s
+
+(*v evaluates to "hd", because it raises the exception Failure "hd"*)
