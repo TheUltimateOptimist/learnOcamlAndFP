3703c6fb1d84746f2900f5b12fd7b45040a34a54
TheUltimateOptimist <digitalConfidence@web.de>
2023-03-02 22:55:20 +0100

    implemented has_key, inorder, preorder, postorder

--- binary_search_tree.ml
+++ binary_search_tree.ml
@@ -61,3 +61,57 @@ let rec _get(node : ('key, 'value) node option)(key: 'key) : 'value option =
                 | _ -> failwith "Duplicate keys are not allowed"
 
 let rec get(bst : ('key, 'value) bst)(key: 'key) = _get bst.root key
+
+let has_key (bst : ('key, 'value) bst)(key: 'key) = 
+  match get bst key with
+    | None -> false
+    | Some _ -> true
+
+let size (bst : ('key, 'value) bst) = 
+  match bst.root with
+    | None -> 0
+    | Some node -> node.size
+
+
+(*the cons opeator has const time complexity, thus the following function is O(n)*)
+let keys_inorder(bst : ('key, 'value) bst) = 
+  let rec keys_inorder (list: 'key list) = function
+    | None -> list
+    | Some node -> 
+      let right_list = keys_inorder list node.right in
+      let middle_list = node.key :: right_list in 
+      keys_inorder middle_list node.left in
+  keys_inorder [] bst.root
+
+
+let keys_preorder(bst : ('key, 'value) bst) = 
+  let rec keys_inorder (list: 'key list) = function
+    | None -> list
+    | Some node -> 
+      let right_list = keys_inorder list node.right in
+      let middle_list = keys_inorder right_list node.left in 
+      node.key :: middle_list in
+  keys_inorder [] bst.root
+
+let keys_postorder(bst : ('key, 'value) bst) = 
+  let rec keys_inorder (list: 'key list) = function
+    | None -> list
+    | Some node -> 
+      let right_list = node.key :: list in
+      let middle_list = keys_inorder right_list node.right in 
+      keys_inorder middle_list node.left in
+  keys_inorder [] bst.root
+
+
+(*this is a really bad implementation of the inorder function using the append operator
+the append operator is O(a) with a being the number of elements in the first list, thus
+this implementation is O((n/2)*n) -> O(n**2) -> this sucks big time 
+*)
+let keys_inorder_inefficient(bst : ('key, 'value) bst) = 
+  let rec keys_inorder = function
+    | None -> []
+    | Some node -> [node.key] @ (keys_inorder node.left) @ (keys_inorder node.right) in
+  keys_inorder bst.root
+
+
+
