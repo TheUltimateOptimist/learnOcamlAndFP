026b97ef3575f239128326830f2c41178d010863
TheUltimateOptimist <digitalConfidence@web.de>
2023-03-08 15:00:35 +0100

    tried red black bst implementation

--- binary_search_tree.ml
+++ binary_search_tree.ml
@@ -124,31 +124,55 @@ let keys_inorder_inefficient(bst : ('key, 'value) bst) =
 let _rotate_left (node: ('key, 'value) node) = 
   match node.right with
     | Some right when _is_red (Some right) -> {right with left = Some {node with right = right.left; color=red}; color = node.color}
-    | _ -> failwith "invalid branch"
+    | _ -> failwith "invalid branch rl"
 
 let _rotate_right (node: ('key, 'value) node) =
   match node.left with
     | Some left when _is_red (Some left) -> {left with right = Some {node with left = left.right; color = red}; color = node.color}
-    | _ -> failwith "invalid branch"
+    | _ -> failwith "invalid branch rr"
+
+let string_of_color = function
+  | true -> "red"
+  | false -> "black"
+
+let string_of_node (node: (('key, 'value) node) option) = 
+  let _string_of_node name key value color = name ^ "<" ^ string_of_int key ^ ", " ^ string_of_int value ^ ", " ^ string_of_color color ^ ">" in
+  match node with 
+  | None -> "None"
+  | Some node -> match node.left, node.right with
+    | None, None -> _string_of_node "EmptyNode" node.key node.value node.color
+    | Some _, None -> _string_of_node "LeftFilledNode" node.key node.value node.color
+    | None, Some _ -> _string_of_node "RightFilledNode" node.key node.value node.color
+    | Some _, Some _ -> _string_of_node "FilledNode" node.key node.value node.color
 
 let _flip_colors (node: ('key, 'value) node) =
+  print_endline (string_of_node (Some node));
+  print_endline (string_of_node node.left);
+  print_endline (string_of_node node.right);
   match node.left, node.right with
     | Some left, Some right when not (_is_red (Some node)) && (_is_red (Some left)) && (_is_red (Some right)) -> 
       let new_left = {left with color = black} in 
       let new_right = {right with color = black} in 
       {node with color = red; left = Some new_left; right = Some new_right}
-    | _ -> failwith "invalid branch"
+    | _ -> failwith "invalid branch fc"
 
-
-let put (bst : ('key, 'value) bst) key value = 
+let put key value (bst : ('key, 'value) bst) = 
+  print_endline "sdfsf";
+  print_endline (string_of_node bst.root);
   let rec _put key value = function
-    | None -> empty_node key value red
+    | None -> if is_empty bst then empty_node key value black else empty_node key value red
     | Some node -> 
       let new_node = match node.key with
         | _key when _key > key -> {node with left = Some (_put key value node.left)}
         | _key when _key < key -> {node with right = Some (_put key value node.right)}
         | _ -> {node with value = value} in
-      {new_node with size = 1 + size {root=new_node.left} + size {root=new_node.right}} in
+      let left_rotated = if _is_red new_node.right && not (_is_red new_node.left) then 
+        _rotate_left new_node else new_node in
+      let right_rotated = match left_rotated.left with None -> left_rotated | Some left -> 
+        if _is_red (Some left) && _is_red left.left then 
+          _rotate_right left_rotated else left_rotated in
+      if _is_red right_rotated.left && _is_red right_rotated.right then
+        _flip_colors right_rotated else right_rotated in
   {root = Some (_put key value bst.root)}
 
 let rec _remove_min (node: ('key, 'value) node) = 
@@ -180,8 +204,8 @@ let remove (bst : ('key, 'value) bst) key =
   let rec _remove (node: ('key, 'value) node option) key = 
     match node with
       | None -> None (*node is none return none*)
-      | Some {key=_key;value = _;right = None;left = _left;size = _} when _key = key -> _left (*node key matches key and right Node is None -> return left Node*)
-      | Some {key=_key;value = _;right = _right;left = None;size = _} when _key = key -> _right (*node key matches key and left Node is None -> return right Node*)
+      | Some {key=_key;value = _;right = None;left = _left;size = _;color=_} when _key = key -> _left (*node key matches key and right Node is None -> return left Node*)
+      | Some {key=_key;value = _;right = _right;left = None;size = _;color=_} when _key = key -> _right (*node key matches key and left Node is None -> return right Node*)
       | Some node -> 
         let new_node = match node.key, node.right with
           | _key, _ when key < _key -> {node with left = _remove node.left key} (*key is smaller than key of node -> go left*)
@@ -190,3 +214,5 @@ let remove (bst : ('key, 'value) bst) key =
           | _ -> failwith "impossible branch" in
         Some {new_node with size = 1 + size({root=new_node.left}) + size({root=new_node.right})} in
   {root = _remove bst.root key}
+
+let tree = empty_bst() |> put 3 3 |> put 5 5 |> put 7 7 |> put 9 9 |> put 11 11 |> put 2 2 |> put 1 1 |> put 100 100 |> put 101 101 |> keys_inorder
